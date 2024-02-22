1bbd9b6a0da83f985c6fbfa69458da114aa765d3
Emmanuel-Oite <143942455+Emmanuel-Oite@users.noreply.github.com>
2024-02-22 16:41:53 +0300

    Completed Assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
-# your code goes here!
+# your code goes here!from typing import List
+
+class Anagram:
+    def __init__(self, word: str):
+        self.word = word.lower()
+        self.sorted_word = ''.join(sorted(self.word))
+
+    def match(self, possible_anagrams: List[str]) -> List[str]:
+        return [word for word in possible_anagrams if self.is_anagram(word)]
+
+    def is_anagram(self, word: str) -> bool:
+        return self.sorted_word == ''.join(sorted(word.lower()))
